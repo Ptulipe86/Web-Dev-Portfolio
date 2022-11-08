cd04cf75c4296308035d71655acd31529a0404cd
Paul Latulipe <platulipe1986@hotmail.com>
2022-11-08 04:41:21 -0500

    Initial setup. Folders cleaned, bootstrap  inst.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
